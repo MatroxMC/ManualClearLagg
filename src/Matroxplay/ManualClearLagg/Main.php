c05a79c59dcc5fddfd68a0f858281de0f1984f6b
Matroxplay <36140949+MatroxplayYTB@users.noreply.github.com>
2019-12-09 18:43:49 +0100

    Update Main.php

--- src/Matroxplay/ManualClearLagg/Main.php
+++ src/Matroxplay/ManualClearLagg/Main.php
@@ -29,7 +29,7 @@ class Main extends PluginBase{
 		        }		
             }
         }
-        if ($rettnt > 1) {
+        if ($rettnt >= 1) {
             $sender->sendMessage("§c>>§r $rettnt TNT and §r$ret items have been removed !");
         }else {
             $sender->sendMessage("§c>>§r $ret items have been removed !");
