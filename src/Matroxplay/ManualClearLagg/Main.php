c895dc31c8f597413deee8c7848dd9d7ce5c3017
Matroxplay <36140949+MatroxplayYTB@users.noreply.github.com>
2019-12-09 18:34:19 +0100

    Update Main.php
    Add PrimedTNT removing

--- src/Matroxplay/ManualClearLagg/Main.php
+++ src/Matroxplay/ManualClearLagg/Main.php
@@ -6,13 +6,15 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
 use pocketmine\entity\object\ItemEntity;
+use pocketmine\entity\object\PrimedTNT;
 
 class Main extends PluginBase{
 
 	public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
 		switch($command->getName()){
 			case "clearlagg":
-				$ret = 0;
+                $ret = 0;
+                $rettnt = 0;
 
         foreach($this->getServer()->getLevels() as $level) {
 			foreach($level->getEntities() as $entity) {
@@ -20,10 +22,18 @@ class Main extends PluginBase{
                     $ret++;
                     $entity->close();       
                 }
+				
+		        if($entity instanceof PrimedTNT){
+                   $rettnt++;
+                   $entity->close(); 
+		        }		
             }
         }
-
-        $sender->sendMessage("§c>> §r$ret items have been removed !");
+        if ($rettnt > 1) {
+            $sender->sendMessage("§c>> $rettnt and §r$ret items have been removed !");
+        }else {
+            $sender->sendMessage("§c>> §r$ret items have been removed !");
+        }
         $ret = 0;
 				return true;
 		}
